9e5bcb173cd954875fefa8c26daf0e453c356894
Ksenia <36692279+KseniaTabakova@users.noreply.github.com>
2019-09-10 00:57:48 +0300

    Create Task03.sql

---
+++ AdditionalTask_02/Task03.sql
@@ -0,0 +1,17 @@
+- Create a query that lists all countries from the dbo.Customers.Country column. 
+- The list should be sorted in alphabetical order and should contain only unique values.
+
+use customers;
+
+DROP TABLE information;
+
+CREATE TABLE information (
+    customerID INT NOT NULL,
+    country VARCHAR(20),
+    PRIMARY KEY(customerID));
+
+INSERT INTO information (customerID, country)
+    VALUES (1, 'Belarus'), (2, 'Italy'), (32, 'USA'), (52, 'Belarus'), (3, 'Angola'), (99, 'USA');
+
+SELECT DISTINCT country FROM information
+    ORDER BY country;
