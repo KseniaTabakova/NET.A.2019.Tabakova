db6c2305630249861635e58ac9599eaaf0b6836b
Ksenia <36692279+KseniaTabakova@users.noreply.github.com>
2019-09-10 00:30:24 +0300

    nigth sql-tasks one love

---
+++ AdditionalTask_02/Task01.sql
@@ -0,0 +1,16 @@
+-- A query displays a list of customers in a table consisting of two columns, 
+-- customerID and companyName. Table rows must be sorted by customer code.
+
+create database customers;
+
+use customers;
+
+CREATE TABLE information (
+    customerID INT,
+    companyName VARCHAR(20));
+
+INSERT INTO information (customerID, companyName)
+    VALUES (1, 'MAC'), (12, 'EPAM'), (8, 'ZARA');
+
+SELECT * FROM information
+    ORDER BY customerID;
