ed0290a244cf8701ee6c19fb38a156a5b6dcd55d
Ksenia <36692279+KseniaTabakova@users.noreply.github.com>
2019-09-11 00:43:56 +0300

    Update Task10.sql

--- AdditionalTask_02/Task10.sql
+++ AdditionalTask_02/Task10.sql
@@ -1,1 +1,5 @@
---h
+-- Query displays the EmployeeID of the penultimate employee hired by the company. 
+-- Use the subquery to exclude the last employee hired.
+
+SELECT employeeID FROM employees 
+     WHERE employmentData = (SELECT MAX(employmentData) FROM employees WHERE employmentData < (SELECT MAX(employmentData) FROM employees));
